db52a2c0e21b2133600533b2c0d8b9ecdc8b2cac
James L. Johnson III <jamesljohnson3@gmail.com>
2024-09-03 21:12:02 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Next.js configuration
+  reactStrictMode: true, // You can add other configurations here
 
-     ignoreBuildErrors: true,
+  typescript: {
+    // Ignore TypeScript build errors
+    ignoreBuildErrors: true,
   },
   eslint: {
+    // Ignore ESLint errors during builds
     ignoreDuringBuilds: true,
-  }
-
-  
+  },
 };
 
 module.exports = nextConfig;
